0f11108d9ba26e4881880bc428adf5d4ffa42bdb
1Parinya <52105816+1Parinya@users.noreply.github.com>
2019-06-29 23:22:42 +0700

    Add files via upload

---
+++ spend1.cpp
@@ -0,0 +1,18 @@
+/*Parinya khaowseang
+ 6206021610102 section : A */
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+   unsigned short b ;
+   float d = 30.71  ;
+   float eu = 35 ;
+
+   cout <<"Enter your monney :";
+   cin >> b;
+   cout << "your money is" << b << " bath. " << endl;
+   cout << b <<" Bath is " << b/d << " dollars. " << endl;
+   cout << b <<" bath is " << b/eu << " Euro. " << endl;
+   return 0;
+}
